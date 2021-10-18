f3a6029335a78c99bee156919ec939fd563a6bf6
Dudi Peer <davidpeer@checkpoint.com>
2021-10-18 18:43:08 +0300

    Fixed Key vault should have purge protection enabled

--- terraform-template/azure/vm-scale-set/windows/service-fabric/2-key-vault.tf
+++ terraform-template/azure/vm-scale-set/windows/service-fabric/2-key-vault.tf
@@ -7,6 +7,7 @@ resource "azurerm_key_vault" "main" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
   sku_name = "standard"
+  purge_protection_enabled = true
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
